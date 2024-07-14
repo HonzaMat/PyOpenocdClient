4d4e367d026e620c424fa5d17388a90e4d084eee
Jan Matyas <info@janmatyas.net>
2024-07-14 23:16:40 +0200

    Add documentation - WIP (multiple commits squashed)

---
+++ doc/py_openocd_client/apidocs.rst
@@ -0,0 +1,46 @@
+API Documentation
+=================
+
+PyOpenocdClient
+---------------
+
+.. automodule:: py_openocd_client
+
+   .. autoclass:: py_openocd_client.PyOpenocdClient
+      :members:
+
+Command result
+--------------
+
+.. autoclass:: py_openocd_client.OcdCommandResult
+   :members:
+
+Other data types
+----------------
+
+.. autoclass:: py_openocd_client.BpInfo
+   :members:
+
+.. autoclass:: py_openocd_client.WpInfo
+   :members:
+
+.. autoenum:: py_openocd_client.BpType
+   :members:
+
+.. autoenum:: py_openocd_client.WpType
+   :members:
+
+Exceptions
+----------
+
+.. autoexception:: OcdCommandError
+   :members:
+
+.. autoexception:: OcdConnectionError
+
+.. autoexception:: OcdCommandTimeout
+
+.. autoexception:: OcdCommandInvalidResponse
+
+.. autoexception:: OcdError
+
--- src/py_openocd_client/errors.py
+++ src/py_openocd_client/errors.py
@@ -5,7 +5,7 @@ from .types import OcdCommandResult
 
 class OcdBaseException(Exception):
     """
-    Base class for exceptions of PyOpenocdClient.
+    Base exception class for all exceptions of PyOpenocdClient.
     """
 
     pass
@@ -13,7 +13,8 @@ class OcdBaseException(Exception):
 
 class OcdCommandFailedError(OcdBaseException):
     """
-    Exception denoting a TCL command that ended unsuccessfully.
+    Exception which denotes that a TCL command failed;
+    that is, the command ended with a non-zero return code.
     """
 
     def __init__(self, result: OcdCommandResult):
@@ -24,10 +25,46 @@ class OcdCommandFailedError(OcdBaseException):
 
     @property
     def result(self) -> OcdCommandResult:
+        """
+        Result of the command -- an instance of
+        :py:class:`py_openocd_client.OcdCommandResult`. It allows to access
+        the error code and the command output.
+
+        Example of use:
+
+        .. code-block:: python
+
+            try:
+                res = ocd.cmd("some command")
+                print(f"Command succeeded. Output: {res.out}")
+            except OcdCommandError as e:
+                print(f"Command failed with error code {e.result.retcode}. "
+                      f"Output: {e.result.out}")
+
+        """
         return self._result
 
 
 class OcdCommandTimeoutError(OcdBaseException):
+    """
+    Exception that is raised whenever a TCL command does not complete
+    within the configured timeout.
+
+    If this exception occurs, it is advisable to:
+
+    - investigate why the command execution takes long time, and/or
+    - re-connect and re-issue the command with a larger timeout.
+
+    .. note::
+        If this exception is raised, then the connection is automatically
+        terminated.
+
+        If the user wishes to continue issuing more commands, new
+        connection needs to be established (by calling ``connect()`` or
+        ``reconnect()``).
+
+    """
+
     def __init__(self, msg: str, raw_cmd: str, timeout: float):
         self._raw_cmd = raw_cmd
         self._timeout = timeout
@@ -36,22 +73,22 @@ class OcdCommandTimeoutError(OcdBaseException):
     @property
     def raw_cmd(self) -> str:
         """
-        Raw command that did not complete within the timeout.
+        Raw command which did not complete within the timeout.
         """
         return self._raw_cmd
 
     @property
     def timeout(self) -> float:
         """
-        Timeout value which got exceeded.
+        Timeout value that got exceeded.
         """
         return self._timeout
 
 
 class OcdInvalidResponseError(OcdBaseException):
     """
-    Exception which means that a TCL command produced unexpected output. That is,
-    PyOpenocdClient could not understand the output form OpenOCD and parse it.
+    Exception which means that a TCL command produced invalid (unexpected) output.
+    That is, PyOpenocdClient could not parse and/or interpret that command output.
     """
 
     def __init__(self, msg: str, raw_cmd: str, out: str):
@@ -69,20 +106,37 @@ class OcdInvalidResponseError(OcdBaseException):
     @property
     def out(self) -> str:
         """
-        The actual response which could not be understood and parsed.
+        The actual invalid response of the command.
         """
         return self._out
 
 
 class OcdConnectionError(OcdBaseException):
+    """
+    Exception that denotes connection errors, for instance:
+
+    - connection to OpenOCD could be established
+    - OpenOCD closed the connection
+    - OpenOCD responded unexpectedly (protocol broken)
+
+    .. note::
+        If this exception is raised, then the connection is automatically
+        terminated.
+
+        If the user wishes to continue issuing more commands, new
+        connection needs to be established (by calling ``connect()`` or
+        ``reconnect()``).
+
+    """
+
     pass
 
 
 class _OcdParsingError(OcdBaseException):
     """
-    Internal exception that denotes parsing error.
+    Internal exception that denotes a parsing error.
 
-    Not part of the public API. May change between versions.
+    Not part of the public API of PyOpenocdClient. May change between releases.
     """
 
     pass
