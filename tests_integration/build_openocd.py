c5d81a571b258f9617922f1bc0227c5923adc75b
Jan Matyas <info@janmatyas.net>
2025-02-09 23:21:43 +0100

    Use more Jim Tcl versions for testing
    The Tcl interface of OpenOCD depends not only on the OpenOCD
    version itself but also on the version of the built-in Jim Tcl
    interpreter.
    
    For that reason, run the integration tests on multiple Jim Tcl
    versions, too.

--- tests_integration/build_openocd.py
+++ tests_integration/build_openocd.py
@@ -8,34 +8,57 @@ import subprocess
 import sys
 from pathlib import Path
 
-REPO_VANILLA = "https://github.com/openocd-org/openocd.git"
-REPO_RISCV = "https://github.com/riscv-collab/riscv-openocd.git"
+REPO_OPENOCD_VANILLA = "https://github.com/openocd-org/openocd.git"
+REPO_OPENOCD_RISCV = "https://github.com/riscv-collab/riscv-openocd.git"
+
+REPO_LIBJIM = "https://github.com/msteveb/jimtcl.git"
+LIBJIM_CONFIGURE_ARGS = ["--with-ext=json", "--minimal", "--disable-ssl"]
+
+LIBJIM_FROM_APT = "apt"
+LIBJIM_FROM_SOURCE_0_83 = "0.83"
 
 BUILD_INFO = {
     "vanilla-v0.10.0": {
-        "repo": REPO_VANILLA,
+        "repo": REPO_OPENOCD_VANILLA,
         "git_rev": "v0.10.0",
         "extra_cflags": "-Wno-error",
+        "libjim_version": None,
     },
     "vanilla-v0.11.0": {
-        "repo": REPO_VANILLA,
+        "repo": REPO_OPENOCD_VANILLA,
         "git_rev": "v0.11.0",
         "extra_cflags": "-Wno-error",
+        "libjim_version": None,
     },
     "vanilla-v0.12.0": {
-        "repo": REPO_VANILLA,
+        "repo": REPO_OPENOCD_VANILLA,
         "git_rev": "v0.12.0",
         "extra_cflags": "",
+        "libjim_version": None,
     },
-    "vanilla-master": {
-        "repo": REPO_VANILLA,
+    "vanilla-master-default-libjim": {
+        "repo": REPO_OPENOCD_VANILLA,
         "git_rev": "master",
         "extra_cflags": "",
+        "libjim_version": LIBJIM_FROM_APT,
     },
-    "riscv-master": {
-        "repo": REPO_RISCV,
+    "vanilla-master-libjim-0.83": {
+        "repo": REPO_OPENOCD_VANILLA,
+        "git_rev": "master",
+        "extra_cflags": "",
+        "libjim_version": LIBJIM_FROM_SOURCE_0_83,
+    },
+    "riscv-master-default-libjim": {
+        "repo": REPO_OPENOCD_RISCV,
         "git_rev": "riscv",
         "extra_cflags": "",
+        "libjim_version": LIBJIM_FROM_APT,
+    },
+    "riscv-master-libjim-0.83": {
+        "repo": REPO_OPENOCD_RISCV,
+        "git_rev": "riscv",
+        "extra_cflags": "",
+        "libjim_version": LIBJIM_FROM_SOURCE_0_83,
     },
 }
 
@@ -49,14 +72,14 @@ def get_script_dir() -> Path:
     return Path(__file__).resolve().parent
 
 
-def get_src_dir(version: str) -> Path:
-    """Return path to the directory with sources of given OpenOCD version."""
-    return initial_work_dir / "src" / version
+def get_src_dir(version: str, program: str) -> Path:
+    """Return path to the directory with sources of OpenOCD or Jim Tcl."""
+    return initial_work_dir / "src" / version / program
 
 
-def get_install_dir(version: str) -> Path:
-    """Return path to the directory where given version of OpenOCD will be installed."""
-    return initial_work_dir / "install" / version
+def get_install_dir(version: str, program: str) -> Path:
+    """Return path to the directory with installed OpenOCD or Jim Tcl."""
+    return initial_work_dir / "install" / version / program
 
 
 def parse_args() -> argparse.Namespace:
@@ -78,10 +101,45 @@ def run_cmd(cmd: list[str], cwd: str | None = None, env=None) -> None:
     subprocess.check_call(cmd, cwd=cwd, env=env)
 
 
-def perform_build(version: str) -> None:
-    src_dir = get_src_dir(version)
+def prepare_libjim(version: str) -> None:
+    libjim_version = BUILD_INFO[version]["libjim_version"]
+
+    if libjim_version is None:
+        return
+    elif libjim_version == LIBJIM_FROM_APT:
+        run_cmd(["sudo", "apt-get", "install", "-y", "libjim-dev"])
+    else:
+        checkout_and_build_libjim(version)
+
+
+def checkout_and_build_libjim(version: str) -> None:
+    src_dir = get_src_dir(version, "libjim")
+    install_dir = get_install_dir(version, "libjim")
     recreate_dir(src_dir)
-    install_dir = get_install_dir(version)
+    recreate_dir(install_dir)
+
+    run_cmd(["git", "clone", REPO_LIBJIM, "."], cwd=src_dir)
+    run_cmd(["git", "checkout", BUILD_INFO[version]["libjim_version"]], cwd=src_dir)
+    run_cmd(
+        ["git", "--no-pager", "show", "--no-patch"], cwd=src_dir
+    )  # show current commit
+
+    configure_cmd = [
+        "/bin/sh",
+        "./configure",
+        "--prefix=" + str(install_dir),
+    ] + LIBJIM_CONFIGURE_ARGS
+    run_cmd(configure_cmd, cwd=src_dir)
+
+    nproc = min(multiprocessing.cpu_count(), 8)  # safety
+    run_cmd(["make", f"-j{nproc}"], cwd=src_dir)
+    run_cmd(["make", "install"], cwd=src_dir)
+
+
+def checkout_and_build_openocd(version: str) -> None:
+    src_dir = get_src_dir(version, "openocd")
+    install_dir = get_install_dir(version, "openocd")
+    recreate_dir(src_dir)
     recreate_dir(install_dir)
 
     run_cmd(["git", "clone", BUILD_INFO[version]["repo"], "."], cwd=src_dir)
@@ -93,8 +151,19 @@ def perform_build(version: str) -> None:
     run_cmd(["/bin/sh", "./bootstrap"], cwd=src_dir)
 
     extra_cflags = BUILD_INFO[version]["extra_cflags"]
+
     env = os.environ.copy()
     env["CFLAGS"] = extra_cflags
+
+    libjim_is_from_source = BUILD_INFO[version]["libjim_version"] not in [
+        None,
+        LIBJIM_FROM_APT,
+    ]
+    if libjim_is_from_source:
+        env["PKG_CONFIG_PATH"] = (
+            get_install_dir(version, "libjim") / "lib" / "pkgconfig"
+        )
+
     run_cmd(
         ["/bin/sh", "./configure", "--prefix=" + str(install_dir)], cwd=src_dir, env=env
     )
@@ -105,7 +174,7 @@ def perform_build(version: str) -> None:
 
 
 def check_build(version: str) -> None:
-    install_dir = get_install_dir(version)
+    install_dir = get_install_dir(version, "openocd")
 
     openocd_bin = install_dir / "bin" / "openocd"
     if not openocd_bin.is_file():
@@ -119,8 +188,11 @@ def check_build(version: str) -> None:
 
 def main() -> int:
     args = parse_args()
-    perform_build(args.version)
+
+    prepare_libjim(args.version)
+    checkout_and_build_openocd(args.version)
     check_build(args.version)
+
     return 0
 
 
